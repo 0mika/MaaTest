a699ed83c4b9861b748ea4efad2036310418f748
hecy1 <2227413136@qq.com>
2025-12-23 17:52:30 +0800

    第一次提交

---
+++ README.md
@@ -0,0 +1,7 @@
+# 描述
+
+- python环境为开发环境，打包之后无需python环境也可运行
+- 下载maaframework解压至deps文件夹中
+- assets\MaaCommonAssets文件夹中放置通用模型，使用模型时导入到model\ocr中
+
+以上文件需额外下载
---
+++ scripts/install.py
@@ -0,0 +1,94 @@
+from pathlib import Path
+import jsonc
+import shutil
+import sys
+
+
+# 复制模型到项目内
+def configure_ocr_model():
+    assets_dir = Path(__file__).parent.parent / "assets"
+    shutil.copytree(
+        assets_dir / "MaaCommonAssets" / "OCR" / "ppocr_v4" / "zh_cn",
+        assets_dir / "resource" / "model" / "ocr",
+        dirs_exist_ok=True,
+    )
+
+
+working_dir = Path(__file__).parent.parent
+install_path = working_dir / Path("install")
+version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
+
+
+def install_deps():
+    if not (working_dir / "deps" / "bin").exists():
+        print('Please download the MaaFramework to "deps" first.')
+        print('请先下载 MaaFramework 到 "deps"。')
+        sys.exit(1)
+
+    shutil.copytree(
+        working_dir / "deps" / "bin",
+        install_path,
+        ignore=shutil.ignore_patterns(
+            "*MaaDbgControlUnit*",
+            "*MaaThriftControlUnit*",
+            "*MaaRpc*",
+            "*MaaHttp*",
+        ),
+        dirs_exist_ok=True,
+    )
+    shutil.copytree(
+        working_dir / "deps" / "share" / "MaaAgentBinary",
+        install_path / "MaaAgentBinary",
+        dirs_exist_ok=True,
+    )
+
+
+def install_resource():
+
+    configure_ocr_model()
+
+    shutil.copytree(
+        working_dir / "assets" / "resource",
+        install_path / "resource",
+        dirs_exist_ok=True,
+    )
+    shutil.copy2(
+        working_dir / "assets" / "interface.json",
+        install_path,
+    )
+
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        interface = jsonc.load(f)
+
+    interface["version"] = version
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        jsonc.dump(interface, f, ensure_ascii=False, indent=4)
+
+
+def install_chores():
+    shutil.copy2(
+        working_dir / "README.md",
+        install_path,
+    )
+    shutil.copy2(
+        working_dir / "LICENSE",
+        install_path,
+    )
+
+
+def install_agent():
+    shutil.copytree(
+        working_dir / "agent",
+        install_path / "agent",
+        dirs_exist_ok=True,
+    )
+
+
+if __name__ == "__main__":
+    install_deps()
+    install_resource()
+    install_chores()
+    install_agent()
+
+    print(f"Install to {install_path} successfully.")
